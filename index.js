bcf5b0a13995c8858e8e3770e049edd3d000f020
Herberts Cruz <herbertscruz@gmail.com>
2019-03-18 13:41:19 -0300

    Initial project

---
+++ index.js
@@ -0,0 +1,8 @@
+  
+// Copyright IBM Corp. 2011,2016. All Rights Reserved.
+// Node module: loopback-single-sql-query
+// This file is licensed under the MIT License.
+// License text available at https://opensource.org/licenses/MIT
+'use strict';
+
+exports.SingleSQLQuery = require('./lib/single-sql-query.js');
